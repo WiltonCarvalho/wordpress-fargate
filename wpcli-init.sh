4ee13141ff0c6bf50cf53d4d5a40fb2dac403986
Wilton Carvalho <wilton.carvalho@riocardmais.com.br>
2022-02-09 22:30:49 -0300

    added db check to init script

--- wpcli-init.sh
+++ wpcli-init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+echo "$(date +"%F %T") [ WordPress Init Script... ]"
 DB_HOST=${DB_HOST:-"localhost"}
 DB_USER=${DB_USER:-"wbuser"}
 DB_PASS=${DB_PASS:-"wppass"}
@@ -10,18 +11,23 @@ WP_PASS=${WP_PASS:-"passw0rd"}
 WP_EMAIL=${WP_EMAIL:-"admin@local.host"}
 WP_TITLE=${WP_TITLE:-"WordPress"}
 WP_DESCRIPTION=${WP_DESCRIPTION:-"Just Another WordPress Blog"}
-chmod g=u,o= /var/www/html
-if [ ! -e /var/www/html/wp-config.php ]; then
+if [ -d /var/www/html ] && [ ! -e /var/www/html/wp-config.php ]; then
+    echo "$(date +"%F %T") [ Waiting for the database to initialize... ]"
+    until mysql --connect-timeout=2 -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME -e 'select 1'; do
+        echo "$(date +"%F %T") [ Waiting for the database to initialize... ]"
+        sleep 3
+    done
     umask 0007
+    echo "$(date +"%F %T") [ WordPress Download... ]"
     gosu 1000:0 wp core download --path="/var/www/html" --quiet
-    echo "sleep 10"
-    sleep 10
     gosu 1000:0 wp config create --path="/var/www/html" --dbhost=$DB_HOST --dbuser=$DB_USER --dbpass=$DB_PASS --dbname=$DB_NAME
-    gosu 1000:0 wp core install --path="/var/www/html" --url=$WP_URL --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --title=$WP_TITLE --skip-email --skip-plugins
+    gosu 1000:0 wp core install --path="/var/www/html" --url=$WP_URL --admin_user=$WP_ADMIN --admin_password=$WP_PASS \
+        --admin_email=$WP_EMAIL --title="$WP_TITLE" --skip-email --skip-plugins
     gosu 1000:0 wp option --path="/var/www/html" update blogdescription "$WP_DESCRIPTION"
     gosu 1000:0 wp rewrite --path="/var/www/html" structure '/%postname%/'
     gosu 1000:0 wp plugin --path="/var/www/html" delete akismet hello
     gosu 1000:0 wp theme --path="/var/www/html" delete twentytwenty twentytwentyone
 fi
+echo "$(date +"%F %T") [ WordPress Installed... ]"
 gosu 1000:0 wp --info
 exec gosu 1000:0 tail -f /dev/null
